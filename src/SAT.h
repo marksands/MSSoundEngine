338a9936572397902772076d83bb957e1773b7d7
mark <marksands07@gmail.com>
2009-09-06 04:04:25 -0500

    Added multiple play source functionality

--- src/SAT.h
+++ src/SAT.h
@@ -1,4 +1,4 @@
-////////////////////////////////////////////////////
+g////////////////////////////////////////////////////
 // Mark Sands
 // September, 4 2009
 // Simple Audio Toolkit
@@ -8,6 +8,7 @@
 #ifndef SAT_H
 #define SAT_H
 
+#include <vector>
 #include <stdio.h>
 #include <stdlib.h>
 #include "al.h"
@@ -15,33 +16,40 @@
 #include "alut.h"
 
 namespace OpenALManager {
-	
-	typedef struct {
-	  char  riff[4];
-	  unsigned int riffSize;
-	  char  wave[4];
-	  char  fmt[4];
-	  unsigned int fmtSize;
-	  unsigned short format;
-	  unsigned short channels;
-	  unsigned int samplesPerSec;
-	  unsigned int bytesPerSec;
-	  unsigned short blockAlign;
-	  unsigned short bitsPerSample;
-	  char  data[4];
-	  unsigned int dataSize;
-	} SimpleWAVHeader;
-	
+		
 	class SAT {
+
 		public:
 			SAT( char* filename = "NA" );
-			SAT( char* filenames[] );
+			SAT( char* filenames[], int size = 1 );
 			virtual ~SAT();
-			void Play( bool flag = AL_TRUE);
+			void Play( bool flag = AL_FALSE);
+			void Play( int num, bool flag = AL_FALSE );
 			void Pause();
+			void Pause( int num );
 			void Stop();
+			void Stop( int num );
+
 		private:
+			// WAVheader
+			typedef struct {
+			  char  riff[4];
+			  unsigned int riffSize;
+			  char  wave[4];
+			  char  fmt[4];
+			  unsigned int fmtSize;
+			  unsigned short format;
+			  unsigned short channels;
+			  unsigned int samplesPerSec;
+			  unsigned int bytesPerSec;
+			  unsigned short blockAlign;
+			  unsigned short bitsPerSample;
+			  char  data[4];
+			  unsigned int dataSize;
+			} SimpleWAVHeader;
+
 			bool Init();
+			bool InitSources();
 			void Delete();
 			
 			char* ReadWAV( char* filename, SimpleWAVHeader* header );
@@ -57,9 +65,18 @@ namespace OpenALManager {
 			ALboolean alLoop;
 			unsigned int alSource;
 			unsigned int alSampleSet;
+
+			ALuint NUM_BUFFERS;
+			ALuint *Buffers;
+			ALuint *Sources;
+			ALfloat (*SourcesPos)[3];
+			ALfloat (*SourcesVel)[3];
+			ALfloat ListenerPos[3];
+			ALfloat ListenerVel[3];
+			ALfloat ListenerOri[6];
 			
 			char* filename;
-			char* filenames[];
+			std::vector<char*> filenames;
 	};
 
 	// Constructor loads filename and data members
@@ -74,10 +91,13 @@ namespace OpenALManager {
 	}
 
 	// Constructor loads filename array of songs and data members
-	SAT::SAT( char* filenames[] ) {
-		//SAT::filenames = filename;
+	SAT::SAT( char* filenames[], int size ) {
+		
+
+		for( int i = 0; i < size; i++ )
+			SAT::filenames.push_back( filenames[i] );
 			
-		if ( !Init() )
+		if ( !InitSources() )
 			std::cerr << "Failed to initialize OpenAL!";
 	}
 
@@ -91,15 +111,31 @@ namespace OpenALManager {
 		alSourcei(alSource, AL_LOOPING, (flag ? AL_TRUE : AL_FALSE) );
 		alSourcePlay(alSource);
 	}
+
+	// OpenAL Play Sound
+	void SAT::Play( int num, bool flag ) {
+		alSourcei(Sources[num], AL_LOOPING, (flag ? AL_TRUE : AL_FALSE) );
+		alSourcePlay(Sources[num]);
+	}
 	
 	// OpenAL Pause Sound
 	void SAT::Pause() {
 		alSourcePause(alSource);
 	}
+
+	// OpenAL Pause Sound
+	void SAT::Pause( int num ) {
+		alSourcePause(Sources[num]);
+	}
 	
 	// OpenAL Stop sound
 	void SAT::Stop() {
 		alSourceStop(alSource);
+	}
+
+	// OpenAL Stop sound
+	void SAT::Stop( int num ) {
+		alSourceStop(Sources[num]);
 	}
 	
 	// initialize OpenAL and render sound
@@ -141,9 +177,87 @@ namespace OpenALManager {
 		return true;
 	}
 
+	// initialize OpenAL and render sound
+	bool SAT::InitSources() {
+
+		NUM_BUFFERS = filenames.size();
+
+		Buffers = new ALuint[NUM_BUFFERS];
+		Sources = new ALuint[NUM_BUFFERS];
+		SourcesPos = new ALfloat[NUM_BUFFERS][3];
+		SourcesVel = new ALfloat[NUM_BUFFERS][3];
+
+		for ( int i = 0; i < NUM_BUFFERS; i++) {
+			for ( int j = 0; j < 3; j++ ) {
+				SourcesPos[i][j] = 0.0;
+				SourcesVel[i][j] = 0.0;
+			}
+		}
+
+		for ( int i = 0; i < 3; i++ )
+			ListenerPos[i] = 0.0;
+		for ( int i = 0; i < 3; i++ )
+			ListenerVel[i] = 0.0;
+
+		ListenerOri[0] = 0.0;
+		ListenerOri[1] = 0.0;
+		ListenerOri[2] = 1.0;
+		ListenerOri[3] = 0.0;
+		ListenerOri[4] = -1.0;
+		ListenerOri[5] = 0.0;
+
+		Device = alcOpenDevice( (ALCchar*)"DirectSound3D" );
+		if (Device == NULL)
+			return false;
+
+		Context = alcCreateContext(Device,NULL);
+		alcMakeContextCurrent(Context);
+		alGetError();
+
+		// Variables to load into.
+		ALenum format;
+		ALsizei size;
+		ALvoid* data;
+		ALsizei freq;
+		ALboolean loop;
+
+		// Load wav data into buffers.
+		alGenBuffers(NUM_BUFFERS, Buffers);
+
+		for ( int i = 0; i < NUM_BUFFERS; i++) {
+			SimpleWAVHeader header;
+			char* data = ReadWAV( filenames[i], &header );
+			Buffers[i] = CreateBufferFromWav( data, header );
+		}
+
+		alGenSources(NUM_BUFFERS, Sources);
+
+		for ( int i = 0; i < NUM_BUFFERS; i++ ) {
+			alSourcefv(Sources[i], AL_POSITION, SourcesPos[0]);	
+			alSourcefv(Sources[i], AL_VELOCITY, SourcesVel[0]);
+
+			alSourceQueueBuffers( Sources[i], 1, &Buffers[i] );
+		}
+
+		alListenerfv(AL_POSITION,    ListenerPos);
+		alListenerfv(AL_VELOCITY,    ListenerVel);
+		alListenerfv(AL_ORIENTATION, ListenerOri);
+	
+		if( alGetError() != AL_NO_ERROR)
+			return AL_FALSE;
+
+		return true;
+	}
+
 	// OpenAL Delete sound	
 	void SAT::Delete() {
 		
+		//delete [] Buffers;
+		//delete [] Sources;
+
+		alDeleteBuffers(NUM_BUFFERS, &Buffers[0]);
+		alDeleteSources(NUM_BUFFERS, &Sources[0]);
+
 		alSourceUnqueueBuffers( alSource, 1, &alSampleSet);		
 		alDeleteSources(1, &alSource);
 		alDeleteBuffers(1, &alSampleSet);
